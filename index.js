2f9b80107d98a4f2ef0a2078106b30a670d1243a
Winston <niera.w.a@gmail.com>
2021-06-02 21:27:02 -0400

    added prompts and index.js file

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const Employees = require('./lib/Employee');
-const Engineer = require('./lib/Engineer');
-const Manager = require('./lib/manager');
-const Intern = require('./lib/intern');
-const inquirer = require('inquirer');
+const employeeGen = require("./lib/employeeGen")
 
+const EmployeeGen = new employeeGen();
+
+employeeGen.init();
