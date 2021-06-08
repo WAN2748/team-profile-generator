094205ee4e989eac1bcdaca3e6c8b20e6c47ac71
Winston <niera.w.a@gmail.com>
2021-06-08 13:33:22 -0400

    added index.js and employeeGen.js

--- index.js
+++ index.js
@@ -1,5 +1,108 @@
-const employeeGen = require("./lib/employeeGen")
+const Employee = require('./lib/employee');
+const Engineer = require('./lib/Engineer');
+const Manager = require('./lib/manager');
+const Intern = require('./lib/intern');
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+
+let newEmployee = [];
+
+
+ 
+   
+addEmployee = () => {
+    inquirer.prompt([
+    {
+        type: "input",
+        name: "name",
+        message: "Enter employee's name",
+        validate: answer => {
+            if (answer !== "") {
+              return true;
+            }
+            return "Please enter at least one character.";
+          }
+
+    },
+    {
+        type: "list",
+        name: "role",
+        message: "Select employee role",
+        choices: [
+            "Engineer",
+            "Intern",
+            "Manager",
+            "Employee"
+        ],
+
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "Enter employee id"
+        
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Enter employee email address"
+        
+    }
+])
+
+        .then = ({ name, role, id, email }) => {
+            let roleInfo;
+            if (role === "Engineer") {
+                roleInfo = "GitHub username";
+            } else if (role === "Intern") {
+                roleInfo = "School Name";
+            } else {
+                roleInfo = "Office Phone Number";
+            }
+            inquirer.prompt([
+                {
+                    message: `Enter employee ${roleInfo}`,
+                    name: "roleInfo"
+                },
+                {
+                    type: "list",
+                    message: "Would you like to add more employees?",
+                    name: "addMoreEmployees",
+                    choices: [
+                        "yes",
+                        "no"
+                    ],
+                }
+            ])
+                .then = () => {
+                    roleInfo,
+                    moreEmployees
+                };
+            let newWorker;
+            if (role === "Engineer") {
+                newWorker = new Engineer(name, email, id, roleInfo);
+            }
+            else if (role === "Intern") {
+                newWorker = new Intern(name, id, email, roleInfo);
+            } else {
+                newWorker = new Manager(name, id, email, roleInfo);
+            }
+            newEmployee.push(newWorker);
+            createHtml(newWorker).then = () => {
+                if (moreEmployees === "yes") {
+                    addEmployee();
+                } /*else {
+                    endHtml();*/
+                }
+            };
+        };
+
+
+        addEmployee();
+
+/*const employeeGen = require("./lib/employeeGen")
 
 const EmployeeGen = new employeeGen();
 
-employeeGen.init();
+employeeGen.init();*/
