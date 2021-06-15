b0b144b6e0fd111f15fa1670193ff4b4634a42bb
Winston <niera.w.a@gmail.com>
2021-06-15 18:13:44 -0400

    working on index.js

--- index.js
+++ index.js
@@ -4,84 +4,66 @@ const Manager = require('./lib/manager');
 const Intern = require('./lib/intern');
 const inquirer = require('inquirer');
 const fs = require('fs');
+const util = require('util'); 
 
+const writeFileAsync = util.promisify(fs.writeFile);
 
 let newEmployee = [];
 
 
- 
-   
-addEmployee = () => {
-    inquirer.prompt([
-    {
-        type: "input",
-        name: "name",
-        message: "Enter employee's name",
-        validate: answer => {
-            if (answer !== "") {
-              return true;
+
+
+const addEmployee = () => {
+   return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Enter employee's name",
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter at least one character.";
             }
-            return "Please enter at least one character.";
-          }
 
-    },
-    {
-        type: "list",
-        name: "role",
-        message: "Select employee role",
-        choices: [
-            "Engineer",
-            "Intern",
-            "Manager",
-            "Employee"
-        ],
+        },
+        {
+            type: "list",
+            name: "role",
+            message: "Select employee role",
+            choices: [
+                "Engineer",
+                "Intern",
+                "Manager",
+                "Employee"
+            ],
 
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "Enter employee id"
-        
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "Enter employee email address"
-        
-    }
-])
+        },
+        {
+            type: "number",
+            name: "id",
+            message: "Enter employee id"
 
-        .then = ({ name, role, id, email }) => {
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Enter employee email address"
+
+        }
+    ])
+
+        .then (({ role, id, email, name, github, officeNumber }) => {
+            
             let roleInfo = "";
             if (role === "Engineer") {
-                inquirer.prompt([
-                    {
-                    message: "Enter Github username",
-                    name: "GitHubUserName",
-                    type: input
-                    }
-                ])
-                roleInfo = "GitHubUserName";
-                
+
+                roleInfo = "GitHub user name";
+
             } else if (role === "Intern") {
-                inquirer.prompt([
-                    {
-                    name: "schoolName",
-                    type: input,
-                    message: "Enter school name",
-                    
-                    }
-                ])
-                roleInfo = "SchoolName";
+                roleInfo = "School name";
             } else {
                 roleInfo = "Office Phone Number";
-                inquirer.prompt([
-                    {
-                    message: "Enter offic number",
-                    name: "OfficePhoneNumber",
-                    type: input
-                    }
-                ])
             }
             inquirer.prompt([
                 {
@@ -98,34 +80,81 @@ addEmployee = () => {
                     ],
                 }
             ])
-                .then = () => {
-                    roleInfo,
-                    moreEmployees
-                };
-            let newWorker;
-            if (role === "Engineer") {
-                newWorker = new Engineer(name, email, id, roleInfo);
-            }
-            else if (role === "Intern") {
-                newWorker = new Intern(name, id, email, roleInfo);
-            } else {
-                newWorker = new Manager(name, id, email, roleInfo);
-            }
-            newEmployee.push(newWorker);
-            createHtml(newWorker).then = () => {
-                if (moreEmployees === "yes") {
+
+            .then (({roleInfo, addMoreEmployees}) => {
+                let moreEmployees;
+                if (role === "Engineer") {
+                    moreEmployees = new Engineer(name, id, email, roleInfo);
+                } else if (role === "Intern") {
+                    moreEmployees = new Engineer(name, id, email, roleInfo);
+                } else (role === "Manager") 
+                    moreEmployees = new Engineer(name, id, email, roleInfo);
+                
+                newEmployee.push(addMoreEmployees);
+                generateHTML(addMoreEmployees);
+            })
+
+            .then ((addMoreEmployees) => {
+                if (addMoreEmployees === "yes") {
                     addEmployee();
-                } /*else {
-                    endHtml();*/
-                }
-            };
-        };
+                } else (addMoreEmployees === "no") 
+                    generateHTML();
+                    console.log("thank you!")
+                
+            }
+            )
+        });
+};
+
+ generateHTML = (anwser) => {
+  return `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.0.1/journal/bootstrap.min.css" 
+  integrity="sha512-nkF1DDLh7XyAdSuLszW4qKJmWiQqPG+G+ysYXZ7nno8Be4qlufWmEVx/DzBvlOLgYTvy/oswPSqfxK7bwj7KTQ==" 
+  crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <title>Document</title>
+</head>
+<body>
+  <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Team Members</h1>
+    <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">${anwser.name}</h5>
+    <ul class="list-group">
+    <li class="list-group-item">Employee ID: ${anwser.id}</li>
+    <li class="list-group-item">Email: ${answer.email}</li>
+  </ul>
+  </div>
+</div>
+    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+    <ul class="list-group">
+      <li class="list-group-item">My GitHub username is ${answer.github}</li>
+      <li class="list-group-item">LinkedIn: ${answers.officeNumber}</li>
+    </ul>
+  </div>
+</div>
+</body>
+</html>`
+};
 
 
-        addEmployee();
+const init = () => {
+    addEmployee()
+      .then((answers) => writeFileAsync('index.html', generateHTML(answers)))
+      .then(() => console.log('Successfully wrote to index.html'))
+      .catch((err) => console.error(err));
+  };
+  
+//   init();
 
 /*const employeeGen = require("./lib/employeeGen")
 
 const EmployeeGen = new employeeGen();
 
 employeeGen.init();*/
+
+init()
